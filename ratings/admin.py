01c522e08ffade0b1c144b62ba30503e3313c88f
marfyl <marfyl.dev@gmail.com>
2019-03-29 13:15:54 +0100

    Custom admin models

--- ratings/admin.py
+++ ratings/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from . import models
-# Register your models here.
 
-admin.site.register(models.Interaction)
+from . import models
+
+
+class InteractionAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'rating',
+        'created', 'edited',
+    )
+
+
+admin.site.register(models.Interaction, InteractionAdmin)
 admin.site.register(models.OverallRating)
 admin.site.register(models.Rating)
 admin.site.register(models.SkipRating)
